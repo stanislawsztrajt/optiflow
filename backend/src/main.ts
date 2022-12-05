73e186887c0947f26fb31fdc3d8baa8c971a7c58
stanislawsztrajt <stanislawsztrajt@wp.pl>
2022-12-05 22:13:59 +0100

    Init app

---
+++ backend/src/main.ts
@@ -0,0 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
+  await app.listen(1337);
+}
+bootstrap();
