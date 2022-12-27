69388c6b8e1991b8fa07154684ed09843e738723
stanislawsztrajt <stanislawsztrajt@wp.pl>
2022-12-27 19:18:58 +0100

    Removed tests code from main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,9 @@
-'use client'
-
 import { featuresRoutes } from "@/utils/data/features-routes";
-import { socket } from "@/utils/socket";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
 
 
 export default function Page() {
-  let oneTime = false;
-  useEffect(() => {
-    if (!oneTime) {
-      oneTime = true
-      socket.emit('joinRoom', { userId: Math.random().toString(), room: '12' })
-        // socket.emit('sendMessage', 'MESSAGE NIE')
-        // socket.on('onlineUsers', (data: any) => console.log(data))
-    }
-  }, [])
-
-  const teset = () => {
-    // socket.emit('sendMessage', 'MESSAGE NIE')
-    // socket.on('message', users => console.log('messages:',users))
-    socket.emit('onlineUsers')
-    socket.on('getOnlineUsers', users => console.log('users: ',users))
-    // socket.on('getMessage', (message) => console.log('get message:', message))
-  }
-
   const routes = featuresRoutes.map((route) => {
     return (
       <div key={route.name} className="flex flex-col items-center p-6 border rounded-input">
@@ -42,7 +21,6 @@ export default function Page() {
 
   return (
     <>
-    <button onClick={teset}>asdfasdf;adklsj</button>
       <div className="flex items-center justify-center w-screen h-screen gap-32">
         {routes}
       </div>
