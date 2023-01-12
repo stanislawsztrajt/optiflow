c8a3e9ce48a3089ad6dff7d26ff39158744ce74b
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-01-12 13:33:36 +0100

    Added loading

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,9 +1,9 @@
-import Loading from "@/features/ui/loading";
-
 export default function LoadingComponent() {
   return (
-    <main className="flex flex-col w-full h-auto lg:h-screen lg:flex-row">
-      <Loading />
-    </main>
+    <div className="flex items-center justify-center h-screen">
+      <div className="relative rounded-full w-28 h-28 animate-spin bg-gradient-to-r from-purple-500 via-blue-500 to-red-500 ">
+        <div className="absolute w-24 h-24 transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 border-2 border-white rounded-full top-1/2 left-1/2"></div>
+      </div>
+    </div>
   );
 }
