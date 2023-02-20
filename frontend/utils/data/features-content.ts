76ac5202b9c49c11fdcb8c5c18e15ca208f4bc23
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-02-20 21:20:32 +0100

    Prod change

--- frontend/utils/data/features-content.ts
+++ frontend/utils/data/features-content.ts
@@ -1,5 +1,5 @@
 export const featuresContent = {
-  books: 'Nie pozwól, by twoje zbędne szkolne książki zalegały na półce - sprzedaj je i wesprzyj swój budżet studencki, a także pomóż innym uczniom w dostępie do potrzebnych im materiałów edukacyjnych.',
+  books: 'Nie pozwól, by twoje zbędne szkolne książki zalegały na półce - sprzedaj je i wesprzyj swój budżet uczniowski, a także pomóż innym uczniom w dostępie do potrzebnych im materiałów edukacyjnych.',
   events: 'Bądź jeden krok przed innymi i zacznij organizować i śledzić szkolne wydarzenia - dzięki temu będziesz mieć pewność, że nic Cię nie zaskoczy.',
   lostItems: 'Pomóż zachować porządek na terenie szkoły i wesprzyj innych uczniów, publikując informacje o znalezionych oraz zgubionych przedmiotach. Dzięki temu możesz pomóc w odnalezieniu ważnych rzeczy i wywrzeć pozytywny wpływ na środowisko szkolne.',
   privateLessons: 'Ucz się w swoim własnym tempie i osiągaj sukces dzięki publikowaniu i korzystaniu z ofert korepetycji. Dzięki temu uzyskasz indywidualne wsparcie od zaufanych kolegów i koleżanek, co pozwoli Ci lepiej zrozumieć trudne zagadnienia i osiągnąć lepsze wyniki w nauce.'
