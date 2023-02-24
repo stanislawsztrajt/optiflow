efcd487d876e1c3a2462776e41b806374afbb201
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-02-24 17:55:46 +0100

    Prod change

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "prettier"
     ],
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
