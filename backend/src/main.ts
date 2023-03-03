51df40caf8ef5dcb45ca02430e03e44ad7391f5f
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-03-03 11:31:36 +0100

    Prod change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
-import { IoAdapter } from '@nestjs/platform-socket.io';
+// import { IoAdapter } from '@nestjs/platform-socket.io';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useWebSocketAdapter(new IoAdapter(app));
+  // app.useWebSocketAdapter(new IoAdapter(app));
+  app.useWebSocketAdapter(new WsAdapter(app))
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
