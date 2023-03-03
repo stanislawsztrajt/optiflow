867ad652dd56b57e5e42951d547181515455ea1f
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-03-03 12:00:15 +0100

    Prod change

--- frontend/utils/socket/index.ts
+++ frontend/utils/socket/index.ts
@@ -2,9 +2,9 @@ import * as io from "socket.io-client";
 import { user } from "../constans/user";
 export const socket = null
 // export const socket = user
-//   ? io.connect(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:1337", {
-//     transports: ['websocket', 'polling'],
-//   })
+  // ? io.connect(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:1337", {
+  //   transports: ['websocket', 'polling'],
+  // })
 //   : null;
 
 // if (socket && user) {
